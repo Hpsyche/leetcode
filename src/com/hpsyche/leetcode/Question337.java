1e090cb963f6e87e49b5e5bb99fa95c149bfbcaf
Hpsyche <460204715@qq.com>
2020-03-24 11:13:51 +0800

    3.24 leetcode

---
+++ src/com/hpsyche/leetcode/Question337.java
@@ -0,0 +1,31 @@
+package com.hpsyche.leetcode;
+
+import com.hpsyche.leetcode.utils.TreeNode;
+
+/**
+ * @author hpsyche
+ * Create on 2020/3/24
+ */
+public class Question337 {
+    public int rob(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        if(root.left!=null){
+            if(root.right!=null) {
+                return Math.max(root.val + rob(root.left.left) + rob(root.left.right) + rob(root.right.left) + rob(root.right.right)
+                        , rob(root.left) + rob(root.right));
+            }else{
+                return Math.max(root.val + rob(root.left.left) + rob(root.left.right)
+                        , rob(root.left) + rob(root.right));
+            }
+        }else{
+            if(root.right!=null){
+                return Math.max(root.val + rob(root.right.left) + rob(root.right.right)
+                        , rob(root.left) + rob(root.right));
+            }else{
+                return Math.max(root.val,rob(root.left)+rob(root.right));
+            }
+        }
+    }
+}
