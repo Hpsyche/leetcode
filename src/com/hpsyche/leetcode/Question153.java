fc58fb42e545d23111fc90819c00a5ecf7fbfdea
Hpsyche <460204715@qq.com>
2020-03-25 12:17:59 +0800

    3.25 leetcode

---
+++ src/com/hpsyche/leetcode/Question153.java
@@ -0,0 +1,28 @@
+package com.hpsyche.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author hpsyche
+ * Create on 2020/3/25
+ */
+public class Question153 {
+    public int findMin(int[] nums) {
+        int l=0;
+        int r=nums.length-1;
+        while (l<r){
+            int mid=(l+r)>>1;
+            if(nums[r]>nums[mid]){
+                r=mid;
+            }else{
+                l=mid+1;
+            }
+        }
+        return nums[l];
+    }
+
+    @Test
+    public void test1(){
+        System.out.println(findMin(new int[]{4,5,6,7,0,1,2}));
+    }
+}
