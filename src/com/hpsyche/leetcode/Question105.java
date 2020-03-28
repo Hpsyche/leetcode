09b170802ed239984c7eec1bc54496f753cebb18
Hpsyche <460204715@qq.com>
2020-03-28 17:53:35 +0800

    3.28 leetcode

---
+++ src/com/hpsyche/leetcode/Question105.java
@@ -0,0 +1,81 @@
+package com.hpsyche.leetcode;
+
+import com.hpsyche.leetcode.utils.TreeNode;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+* @author hpsyche
+* Create on 2020/3/28
+*/
+public class Question105 {
+public TreeNode buildTree(int[] preorder, int[] inorder) {
+    if (preorder.length == 0 || inorder.length == 0) {
+        return null;
+    }
+    TreeNode root = new TreeNode(preorder[0]);
+    for (int i = 0; i < inorder.length; i++) {
+        if (preorder[0] == inorder[i]) {
+            int[] preLeft = Arrays.copyOfRange(preorder, 1, i + 1);
+            int[] preRight = Arrays.copyOfRange(preorder, i + 1, preorder.length);
+            int[] inoLeft = Arrays.copyOfRange(inorder, 0, i);
+            int[] inoRight = Arrays.copyOfRange(inorder, i + 1, inorder.length);
+            root.left = buildTree(preLeft, inoLeft);
+            root.right = buildTree(preRight, inoRight);
+            break;
+        }
+    }
+    return root;
+
+}
+
+
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+    if (preorder.length == 0 || inorder.length == 0) {
+        return null;
+    }
+    return helper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+}
+
+    private TreeNode helper(int[] preorder, int preB, int preE, int[] inorder, int inoB, int inoE) {
+        if (preB > preE || inoB > inoE) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preB]);
+        int i=0;
+        for(;i<=inoE;i++){
+            if(inorder[inoB+i]==preorder[preB]){
+                break;
+            }
+        }
+        root.left = helper(preorder, preB + 1, preB + i, inorder, inoB, inoB + i - 1);
+        root.right = helper(preorder, preB + i + 1, preE, inorder, inoB + i + 1, inoE);
+        return root;
+    }
+
+
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    @Test
+    public void test1(){
+        lengthOfLongestSubstring("abcssda");
+    }
+}
---
+++ src/com/hpsyche/leetcode/Question820.java
@@ -0,0 +1,69 @@
+package com.hpsyche.leetcode;
+
+import java.util.*;
+
+/**
+ * @author hpsyche
+ * Create on 2020/3/28
+ */
+public class Question820 {
+    /**
+     * 解法1
+     *
+     * @param words
+     * @return
+     */
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        for (String word : words) {
+            for (int i = 1; i < word.length(); i++) {
+                set.remove(word.substring(i));
+            }
+        }
+        int count = 0;
+        for (String temp : set) {
+            count += temp.length() + 1;
+        }
+        return count;
+    }
+
+    /**
+     * 解法2
+     * @param words
+     * @return
+     */
+    public int minimumLengthEncoding2(String[] words) {
+       TrieTree trie=new TrieTree();
+       Map<TrieTree,Integer> map=new HashMap<>();
+       for(String word:words){
+           TrieTree curr=trie;
+           for(int j=word.length()-1;j>=0;j--){
+               curr=curr.get(word.charAt(j));
+           }
+           map.put(curr,word.length());
+       }
+       int res=0;
+       for(TrieTree trieTree:map.keySet()){
+           if(trieTree.count==0){
+               res+=map.get(trieTree)+1;
+           }
+       }
+       return res;
+    }
+
+    class TrieTree{
+        int count;
+        TrieTree[] children;
+        TrieTree(){
+            count=0;
+            children=new TrieTree[26];
+        }
+        public TrieTree get(char c){
+            if(children[c-'a']==null){
+                children[c-'a']=new TrieTree();
+                count++;
+            }
+            return children[c-'a'];
+        }
+    }
+}
