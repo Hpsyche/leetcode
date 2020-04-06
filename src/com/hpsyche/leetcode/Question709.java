a2820296c255f424eb26f26197d644dadddada2a
Hpsyche <460204715@qq.com>
2020-04-06 21:03:41 +0800

    4.6 leetcode

---
+++ src/com/hpsyche/leetcode/Question709.java
@@ -0,0 +1,24 @@
+package com.hpsyche.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author hpsyche
+ * Create on 2020/4/6
+ */
+public class Question709 {
+    public String toLowerCase(String str) {
+        char[] chars = str.toCharArray();
+        for(int i=0;i<chars.length;i++){
+            if(chars[i]>='A'&&chars[i]<='Z'){
+                chars[i]+=32;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    @Test
+    public void test1(){
+        System.out.println(toLowerCase("aAdhasoihBDHAOIdhasoi"));
+    }
+}
