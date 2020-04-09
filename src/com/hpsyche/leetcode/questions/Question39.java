1d1e73a58ecdb7ed96684b8eb4eb719092baa71c
Hpsyche <460204715@qq.com>
2020-04-09 14:55:21 +0800

    4.9 leetcode

---
+++ src/com/hpsyche/leetcode/questions/Question39.java
@@ -0,0 +1,36 @@
+package com.hpsyche.leetcode.questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author hpsyche
+ * Create on 2020/4/9
+ */
+public class Question39 {
+    List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        dfs(candidates,target,0,new ArrayList<>());
+        return res;
+    }
+
+    private void dfs(int[] candidates, int target, int i, ArrayList<Integer> tempList) {
+        if(target<0){
+            return;
+        }
+        if(target==0){
+            res.add(new ArrayList<>(tempList));
+            return;
+        }
+        for(int start=i;start<candidates.length;start++){
+            if(target-candidates[start]<0){
+                break;
+            }
+            tempList.add(candidates[start]);
+            dfs(candidates,target-candidates[start],start,tempList);
+            tempList.remove(tempList.size()-1);
+        }
+    }
+}
