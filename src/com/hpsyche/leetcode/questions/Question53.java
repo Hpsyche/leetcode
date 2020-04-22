2e26f66eeb6362442ff3745453af7dad0e06e32e
Hpsyche <460204715@qq.com>
2020-04-22 13:52:44 +0800

    4.22 leetcode

---
+++ src/com/hpsyche/leetcode/questions/Question53.java
@@ -0,0 +1,28 @@
+package com.hpsyche.leetcode.questions;
+
+import org.junit.Test;
+
+/**
+ * @author hpsyche
+ * Create on 2020/4/22
+ */
+public class Question53 {
+    public int maxSubArray(int[] nums) {
+        int res=nums[0];
+        int sum=0;
+        for(int num:nums){
+            if(sum>0){
+                sum+=num;
+            }else{
+                sum=num;
+            }
+            res=Math.max(res,sum);
+        }
+        return res;
+    }
+
+    @Test
+    public void test1(){
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));;
+    }
+}
