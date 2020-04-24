471844b96dc5f9d041874ac5701f044c7e155e12
Hpsyche <460204715@qq.com>
2020-04-24 13:03:16 +0800

    4.24 leetcode

---
+++ src/com/hpsyche/leetcode/questions/Question73.java
@@ -0,0 +1,43 @@
+package com.hpsyche.leetcode.questions;
+
+/**
+ * @author hpsyche
+ * Create on 2020/4/24
+ */
+public class Question73 {
+    public void setZeroes(int[][] matrix) {
+        if(matrix==null||matrix.length==0||matrix[0].length==0){
+            return;
+        }
+        int m=matrix.length;
+        int n=matrix[0].length;
+        boolean row0=false;
+        boolean col0=false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(matrix[i][j]==0){
+                    if(i==0){
+                        row0=true;
+                    }
+                    if(j==0){
+                        col0=true;
+                    }
+                    matrix[0][j]=matrix[i][0]=0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (col0) {
+            for (int i = 0; i < m; i++) {matrix[i][0] = 0;}
+        }
+        if (row0) {
+            for (int j = 0; j < n; j++) {matrix[0][j] = 0;}
+        }
+    }
+}
---
+++ src/com/hpsyche/leetcode/questions/Question74.java
@@ -0,0 +1,82 @@
+package com.hpsyche.leetcode.questions;
+
+import org.junit.Test;
+
+/**
+ * @author hpsyche
+ * Create on 2020/4/24
+ */
+public class Question74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null||matrix.length==0||matrix[0].length==0){
+            return false;
+        }
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int top=0;
+        int bottom=m-1;
+        while (top<=bottom){
+            int mid=top+((bottom-top)>>1);
+            if(matrix[mid][0]<target){
+                top=mid+1;
+            }else if(matrix[mid][0]>target){
+                bottom=mid-1;
+            }else{
+                return true;
+            }
+        }
+        if(bottom<0){
+            return false;
+        }
+        int left=0;
+        int right=n-1;
+        while (left<=right){
+            int mid=left+((right-left)>>1);
+            if(matrix[bottom][mid]<target){
+                left=mid+1;
+            }else if(matrix[bottom][mid]>target){
+                right=mid-1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        if(matrix==null||matrix.length==0||matrix[0].length==0){
+            return false;
+        }
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int left=0;
+        int right=m*n-1;
+        while (left<=right){
+            int mid=left+((right-left)>>1);
+            int midElement=matrix[mid/n][mid%n];
+            if(midElement==target){
+                return true;
+            }else if(midElement>target){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return false;
+    }
+
+
+    @Test
+    public void test1(){
+//        int[][] matrix=new int[][]{
+//                {1,3,5,6},{11,13,14,15},{16,17,19,20},{26,27,29,45}
+//        };
+        int[][] matrix=new int[][]{
+                {1}
+        };
+
+        boolean b = searchMatrix(matrix, 0);
+        System.out.println(b);
+    }
+}
