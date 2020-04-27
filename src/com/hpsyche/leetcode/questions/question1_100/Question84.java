fc10f93fb2d8afa17acd80485e4305220c1cd588
Hpsyche <460204715@qq.com>
2020-04-27 13:01:18 +0800

    4.27 leetcode

---
+++ src/com/hpsyche/leetcode/questions/question1_100/Question84.java
@@ -0,0 +1,43 @@
+package com.hpsyche.leetcode.questions.question1_100;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * @author hpsyche
+ * Create on 2020/4/27
+ */
+public class Question84 {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea=0;
+        for (int i = 0; i < heights.length; i++) {
+            int minHeight=heights[i];
+            for (int j = i; j < heights.length; j++) {
+                minHeight=Math.min(minHeight,heights[j]);
+                maxArea=Math.max(minHeight*(j-i+1),maxArea);
+            }
+        }
+        return maxArea;
+    }
+    public int largestRectangleArea2(int[] heights) {
+        Stack< Integer > stack = new Stack <> ();
+        stack.push(-1);
+        int maxarea = 0;
+        for (int i = 0; i < heights.length; ++i) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
+                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+            }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+        }
+        return maxarea;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(largestRectangleArea2(new int[]{6,7,5,2,4,5,9,3}));;
+    }
+}
