b3217de360d56c2550c154e7207bb69f84050b4e
Hpsyche <460204715@qq.com>
2020-05-10 14:32:58 +0800

    5.10 leetcode

--- src/com/hpsyche/leetcode/questions/question101_200/Question116.java
+++ src/com/hpsyche/leetcode/questions/question101_200/Question116.java
@@ -1,61 +1,61 @@
-//package com.hpsyche.leetcode.questions.question101_200;
-//
-//import com.sun.org.apache.regexp.internal.RE;
-//
-//import java.util.LinkedList;
-//import java.util.Queue;
-//
-///**
-// * @author hpsyche
-// * Create on 2020/5/10
-// */
-//public class Question116 {
-//    class Node {
-//        public int val;
-//        public Node left;
-//        public Node right;
-//        public Node next;
-//
-//        public Node() {}
-//
-//        public Node(int _val) {
-//            val = _val;
-//        }
-//
-//        public Node(int _val, Node _left, Node _right, Node _next) {
-//            val = _val;
-//            left = _left;
-//            right = _right;
-//            next = _next;
-//        }
-//    }
-//
-//    public Node connect(Node root) {
-//        if(root==null){
-//            return null;
-//        }
-//        Queue<Node> queue=new LinkedList<>();
-//        queue.add(root);
-//        Node prev=null;
-//        while (!queue.isEmpty()){
-//            int count=queue.size();
-//            while (count>0) {
-//                Node poll = queue.poll();
-//                count--;
-//                poll.next=prev;
-//                if(count>0){
-//                    prev=poll;
-//                }else{
-//                    prev=null;
-//                }
-//                if(poll.right!=null){
-//                    queue.add(poll.right);
-//                }
-//                if (poll.left!=null){
-//                    queue.add(poll.left);
-//                }
-//            }
-//        }
-//        return root;
-//    }
-//}
+package com.hpsyche.leetcode.questions.question101_200;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author hpsyche
+ * Create on 2020/5/10
+ */
+public class Question116 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        if(root==null){
+            return null;
+        }
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(root);
+        Node prev=null;
+        while (!queue.isEmpty()){
+            int count=queue.size();
+            while (count>0) {
+                Node poll = queue.poll();
+                count--;
+                poll.next=prev;
+                if(count>0){
+                    prev=poll;
+                }else{
+                    prev=null;
+                }
+                if(poll.right!=null){
+                    queue.add(poll.right);
+                }
+                if (poll.left!=null){
+                    queue.add(poll.left);
+                }
+            }
+        }
+        return root;
+    }
+}
